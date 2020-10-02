083e364134ce16944030e6cb7e868a3d3b658f84
Karuna Keshari <karunakeshari884@gmail.com>
2020-10-02 23:20:19 +0530

    user input can be taken

--- SwapTwoNumbers.c
+++ SwapTwoNumbers.c
@@ -1,14 +1,13 @@
-#include<stdio.h>  
+#include<stdio.h>
 
-int main()    
-{    
-int a=10, b=20;      
- printf("Before swap a=%d b=%d",a,b);      
-
-    a=a^b;  
-    b=a^b;    
-    a=a^b;  
-
- printf("\nAfter swap a=%d b=%d",a,b);    
-return 0;  
-}   
+int main()
+{
+int a,b;
+printf("enter two numbers for swapping\n");
+scanf("%d%d",&a,&b);
+int temp=a;
+a=b;
+b=temp;
+ printf("After swap a=%d b=%d",a,b);
+return 0;
+}
